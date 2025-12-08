c836440758a70b4d7995ec83d2a6758327c4b778
hyunsik2000 <ryuhsik2000@naver.com>
2025-12-09 04:26:16 +0900

    feat: alias로 절대 경로 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,17 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@assets": "/src/assets",
+      "@components": "/src/components",
+      "@constants": "/src/constants",
+      "@data": "/src/data",
+      "@hooks": "/src/hooks",
+      "@pages": "/src/pages",
+      "@store": "/src/store",
+      "@utils": "/src/utils",
+    },
+  },
 });
