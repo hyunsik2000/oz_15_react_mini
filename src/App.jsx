4deeb11aa7dba066a058543ee8cf12f5b99e9145
hyunsik2000 <ryuhsik2000@naver.com>
2025-12-09 04:28:24 +0900

    feat: dark 모드 실행을 위한 코드 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/Home";
-import Layout from "./components/Layout";
-import Detail from "./pages/Detail";
-import { Search } from "./pages/Search";
+import Home from "@pages/Home";
+import Layout from "@components/Layout";
+import Detail from "@pages/Detail";
+import { Search } from "@pages/Search";
+import { useThemeStore } from "@store/useThemeStore";
+import { useEffect } from "react";
 
 function App() {
+  const { theme } = useThemeStore();
+
+  useEffect(() => {
+    const root = document.documentElement;
+    root.classList.remove("dark", "light");
+    root.classList.add(theme);
+  }, [theme]);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
